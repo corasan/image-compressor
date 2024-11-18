af56909bed9c9895dfc24e87cb0266bd8c9ec37c
Henry Paulino <corasan.dev@gmail.com>
2024-11-17 21:49:32 -0500

    clangd

--- package/cpp/HybridImageCompressor.cpp
+++ package/cpp/HybridImageCompressor.cpp
@@ -1,6 +1,10 @@
 #include "HybridImageCompressor.hpp"
-#include "ImageUtils.hpp"
+#include <ImageUtils.hpp>
 #include <fstream>
+#include <ios>
+#include <optional>
+#include <stdexcept>
+#include <string>
 #include <vector>
 
 namespace margelo::nitro::imagecompressor {
@@ -18,13 +22,13 @@ std::string HybridImageCompressor::compress(const std::string& uri, const std::o
     if (path.find("file://") == 0) {
         path = path.substr(7);
     }
-    
+
     // Read file into buffer
     std::ifstream file(path, std::ios::binary | std::ios::ate);
     if (!file.is_open()) {
         throw std::runtime_error("Cannot open file: " + path);
     }
-    
+
     // Get file size and read into buffer
     std::streamsize size = file.tellg();
     file.seekg(0, std::ios::beg);
@@ -37,7 +41,7 @@ std::string HybridImageCompressor::compress(const std::string& uri, const std::o
     auto dimensions = ImageUtils::getJPEGDimensions(buffer);
 
     // For now just return debug info
-    return "Loaded image " + path + 
+    return "Loaded image " + path +
            "\nSize: " + std::to_string(size) + " bytes" +
            "\nDimensions: " + std::to_string(dimensions.width) + "x" + std::to_string(dimensions.height);
 }
