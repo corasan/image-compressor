0d6f75494d89c461b2494c97823a063dae36607f
Henry Paulino <corasan.dev@gmail.com>
2025-01-02 15:01:01 +0000

    generated files

---
+++ package/src/index.d.ts
@@ -0,0 +1,3 @@
+import type { ImageCompressor as ImageCompressorType, ImageAsset, CompressedImageAsset, CompressionOptions } from './specs/ImageCompressor.nitro';
+export declare const ImageCompressor: ImageCompressorType;
+export type { ImageAsset, CompressedImageAsset, CompressionOptions };
