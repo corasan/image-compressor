951b19347bc093cf98a9854e1ac916616b080fac
Henry Paulino <corasan.dev@gmail.com>
2025-01-09 13:06:38 +0000

    Delete index.d.ts

--- package/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ImageCompressor as ImageCompressorType, ImageAsset, CompressedImageAsset, CompressionOptions } from './specs/ImageCompressor.nitro';
-export declare const ImageCompressor: ImageCompressorType;
-export type { ImageAsset, CompressedImageAsset, CompressionOptions };
