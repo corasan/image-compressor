6798a32f18cd6f0cec1440eeed86c0e899557341
Henry Paulino <corasan.dev@gmail.com>
2025-01-10 17:41:25 +0000

    Update ImageUtils.hpp

--- package/cpp/ImageUtils.hpp
+++ package/cpp/ImageUtils.hpp
@@ -12,8 +12,8 @@ namespace margelo::nitro::imagecompressor {
   class ImageUtils {
   public:
     // Main compression function
-    static std::shared_ptr<HybridCompressedImageAsset> compressImage(const ImageAsset& image,
-                                                                     const std::optional<CompressionOptions>& options);
+    static std::shared_ptr<HybridCompressedImageAsset>
+    compressImage(const ImageAsset& image, const std::optional<CompressionOptions>& options);
 
     // Helper functions
     static std::string stripFilePrefix(const std::string& path);
@@ -23,8 +23,10 @@ namespace margelo::nitro::imagecompressor {
     static cv::Mat loadImage(const std::string& path);
     static cv::Mat resizeImage(const cv::Mat& img, const std::optional<double>& maxWidth,
                                const std::optional<double>& maxHeight);
-    static std::pair<std::vector<int>, std::string> getCompressionParams(double quality, const std::string& format);
-    static std::filesystem::path generateTempPath(const std::string& originalPath, const std::string& extension);
+    static std::pair<std::vector<int>, std::string> getCompressionParams(double quality,
+                                                                         const std::string& format);
+    static std::filesystem::path generateTempPath(const std::string& originalPath,
+                                                  const std::string& extension);
   };
 
 } // namespace margelo::nitro::imagecompressor
